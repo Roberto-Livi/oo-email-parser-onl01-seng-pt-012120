9e89061f9ab741fd1a12bb612636e23da4f9aa29
Roberto Livi <robertomlivi@outlook.com>
2020-02-04 19:19:58 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,2 +1,20 @@
 require 'pry'
 
+class EmailParser
+  attr_accessor :email
+  EMAIL_ADDRESSES = []
+
+  def initialize(email)
+    @email = email
+    EMAIL_ADDRESSES << email
+  end
+
+  def parse
+    email_parse = email.split.collect do |email|
+      email.split(",")
+    end
+    return_emails = email_parse.flatten.uniq
+    binding.pry
+    return_emails
+  end
+end
